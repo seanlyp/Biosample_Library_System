6badcc10452fe0ba364dd0f85bb5dfacf744a9ac
warku123 <warku123@users.noreply.github.com>
2020-07-15 16:04:42 +0800

    Create DispatchRecord.java
    增加出库记录表

---
+++ src/com/bsb/model/DispatchRecord.java
@@ -0,0 +1,9 @@
+package com.bsb.model;
+//出库记录
+public class DispatchRecord {
+	private String order;				// 出库单号
+	private String time;				// 出库时间
+	private String manager;				// 出库管理员
+	private String application_order;	// 出库申请单号
+	private String state;				// 出库状态
+}
