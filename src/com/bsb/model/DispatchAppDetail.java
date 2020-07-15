d2063b95b21f5bfa54c80e0872b1f16b8db4b6ee
warku123 <warku123@users.noreply.github.com>
2020-07-15 14:50:48 +0800

    Create DispatchAppDetail.java
    创建第一个model，与数据库中表对应建立表项

---
+++ src/com/bsb/model/DispatchAppDetail.java
@@ -0,0 +1,10 @@
+package com.bsb.model;
+
+public class DispatchAppDetail {
+	private String order_num;
+	private String sample_type;
+	private int sample_num;
+	private String sample_condition;
+	private String result;
+	private String reason_or_pos;
+}
