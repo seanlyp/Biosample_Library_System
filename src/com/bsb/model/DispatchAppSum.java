e35284007fee246fbcda2dda5e9acd122d165296
warku123 <warku123@users.noreply.github.com>
2020-07-15 15:59:23 +0800

    Create DispatchAppSum.java
    新建出库申请汇总表

---
+++ src/com/bsb/model/DispatchAppSum.java
@@ -0,0 +1,9 @@
+package com.bsb.model;
+
+public class DispatchAppSum {
+	private String order;		// 出库申请单号
+	private String applicant;	// 出库申请单位
+	private String time;		// 出库申请时间
+	private String result;		// 出库申请结果
+	private String auditor;		// 出库审核员
+}
