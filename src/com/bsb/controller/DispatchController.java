cf318881396ffeb3890d7f33b11b9e1a18046abe
warku123 <warku123@users.noreply.github.com>
2020-07-16 14:22:10 +0800

    增加DispatchApply页面
    增加DispatchApply页面

---
+++ src/com/bsb/controller/DispatchController.java
@@ -0,0 +1,16 @@
+package com.bsb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/dispatch")
+public class DispatchController {
+	
+	@RequestMapping("/apply")
+	public ModelAndView getApply(){
+        ModelAndView mv = new ModelAndView("DispatchApply");
+        return mv;
+    }
+}
