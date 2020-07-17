ea697b3334434d186bda37d304b8f6ec169598e8
warku123 <warku123@users.noreply.github.com>
2020-07-17 10:04:43 +0800

    增加一个页面上的功能
    增加一个页面上的功能

--- src/com/bsb/controller/DispatchController.java
+++ src/com/bsb/controller/DispatchController.java
@@ -1,12 +1,21 @@
 package com.bsb.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bsb.service.DispatchService;
+
 @Controller
 @RequestMapping("/dispatch")
 public class DispatchController {
+	
+	@Autowired
+    private DispatchService dispatchService;
 	
 	@RequestMapping("/apply")
 	public ModelAndView getApply(){
@@ -14,4 +23,38 @@ public class DispatchController {
         return mv;
     }
 	
+	@RequestMapping("/apply_deliversum")
+	public ModelAndView getApplySum(String applicant, String type, String snum, String condition){
+		ModelAndView mv = new ModelAndView("DispatchApply");
+		mv.addObject("applicant", applicant);
+		mv.addObject("type", type);
+		mv.addObject("snum", snum);
+		mv.addObject("condition", condition);
+		
+		String[] types = type.split(",");
+		String[] snums = snum.split(",");
+		String[] conditions = condition.split(",");
+
+        int anum = dispatchService.getApplyNum();
+		anum = anum + 1;
+		String order = "DA" + String.valueOf(anum);
+		
+		String result = "待审核";
+		
+		for(int i = 0; i < types.length; i++) {
+			int num = Integer.valueOf(snums[i]);
+			type = types[i];
+			condition = conditions[i];
+			dispatchService.insertApplyDetail(order, type, num, condition, result); 
+		}
+		
+		
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time = sFormat.format(new Date());
+		
+		dispatchService.insertApplySummary(order, applicant, time, result);
+        
+        // 返回界面
+        return(new ModelAndView("redirect:app_checksum"));
+    }
 }
--- src/com/bsb/service/DispatchService.java
+++ src/com/bsb/service/DispatchService.java
@@ -8,23 +8,16 @@ import com.bsb.model.DispatchRecord;
 
 public interface DispatchService {
 	
-	public List<DispatchAppSum> findAllApp();
-	// 显示指定单号的出库申请
-	public List<DispatchAppSum> findAppByOrder(String order);
-	// 申请单位
-	public List<DispatchAppSum> findAppByApplicant(String applicant);
-	// 时间
-	public List<DispatchAppSum> findAppByTime(String time);
-	// 申请结果
-	public List<DispatchAppSum> findAppByResult(String result);
-	// 审核员
-	public List<DispatchAppSum> findAppByAuditor(String auditor);
-	// 回填申请表审核结果
-	public void appCheckFinal(String order, String result, String auditor);
-	// 获取当前订单数量用于订单编号
-	public Integer getApplyNum();
-	public void insertApplySum(String order, String applicant, String time, String result);
-	// 出库样本(调用出库申请详情表)
-	// public List<DispatchAppSum> findAppBySample(String type);
+	//Apply Detail
+	public void insertApplyDetail(String order, String type, int num, String condition, String result);
+	// 显示指定单号出库申请详情
+	public List<DispatchAppDetail> findAppDetailByOrder(String order);
+	// 填写详情表
+	public void appCheck(String order, String name, String result, String reason);
+	// 指定单显示出库位置详情
+	public List<String> getPosByOrder(String order);
 	
+	public void insertApplySummary(String order, String applicant, String time, String result);
+	//获得订单编号
+	public Integer getApplyNum();
 }
